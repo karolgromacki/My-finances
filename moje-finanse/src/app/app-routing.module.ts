2327b583927a2bc15427089289dc1cf6ce7a8511
Karol Gromacki <karol.gromacki@gmail.com>
2020-10-22 00:24:27 +0200

    Adjusting main routing configuration

--- moje-finanse/src/app/app-routing.module.ts
+++ moje-finanse/src/app/app-routing.module.ts
@@ -2,6 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '', redirectTo: 'categories', pathMatch: 'full'
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./categories/categories.module').then(m => m.CategoriesPageModule)
+  },
+  {
+    path: 'transactions',
+    loadChildren: () => import('./transactions/transactions.module').then(m => m.TransactionsPageModule)
+  },
+  {
+    path: 'accounts',
+    loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsPageModule)
+  },
+
 ];
 
 @NgModule({
