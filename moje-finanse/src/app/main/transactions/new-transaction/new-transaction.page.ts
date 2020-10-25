f3922d5797a5fa81c9d26a77a623d393a5ec7c4e
Karol Gromacki <karol.gromacki@gmail.com>
2020-10-25 16:33:14 +0100

    New Transaction Form

--- moje-finanse/src/app/main/transactions/new-transaction/new-transaction.page.ts
+++ moje-finanse/src/app/main/transactions/new-transaction/new-transaction.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-transaction',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-transaction.page.scss'],
 })
 export class NewTransactionPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      type: new FormControl(null, { updateOn: 'change', validators: [Validators.required] }),
+      title: new FormControl(null, { updateOn: 'change', validators: [Validators.required, Validators.maxLength(20)] }),
+      amount: new FormControl(null, { updateOn: 'change', validators: [Validators.required, Validators.min(0.01)] }),
+      note: new FormControl(null, { updateOn: 'blur' }),
+      date: new FormControl(new Date(Date.now()).toISOString(), { updateOn: 'change', validators: [Validators.required] }),
+      account: new FormControl(null, { updateOn: 'change', validators: [Validators.required] }),
+      category: new FormControl(null, { updateOn: 'change', validators: [Validators.required] }),
+    })
   }
-
+  onCreateOffer() {
+    console.log(this.form);
+  }
 }
